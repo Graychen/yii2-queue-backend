d9b78869f9bc353d0c2b0b3136b8671be1bb884a
陈家辉 <chenjiahui@haolianluo.com>
2017-11-06 17:52:28 +0800

    feat(src): 添加job目录

--- src/jobs/Job.php
+++ src/jobs/Job.php
@@ -9,21 +9,7 @@ use graychen\yii2\queue\backend\models\Queue as QueueDb;
 abstract class Job extends JobAbstract
 {
 
-    public function __construct()
-    {
-        Yii::$app->queue->on(Queue::EVENT_AFTER_PUSH, function ($event) {
-            $queue = new QueueDb();
-            $queue->name=$this->getName();
-            var_dump($queue->name);
-            $queue->catalog=$this->getCatalog();
-            var_dump($queue->catalog);
-            $queue->description=$this->getDescription();
-            var_dump($queue->description);
-            $queue->execTime=$this->getExecTime();
-            var_dump($queue->execTime);
-            $this->save();
-        });
-    }
+
 
     /** name (任务名称)
      * @return mixed
@@ -41,21 +27,7 @@ abstract class Job extends JobAbstract
         return $this->catalog;
     }
 
-    /** description (详情信息)
-     * @return mixed
-     */
-    public function getDescription()
-    {
-        return $this->description;
-    }
 
-    /**
-     * @return mixed  int  时间戳
-     */
-    public function getExecTime()
-    {
-        return $this->execTime;
-    }
 
 
 }
--- src/jobs/JobAbstract.php
+++ src/jobs/JobAbstract.php
@@ -2,12 +2,55 @@
 
 namespace graychen\yii2\queue\backend\jobs;
 use yii\base\Object;
+use yii;
+use yii\queue\Queue;
+use graychen\yii2\queue\backend\models\Queue as QueueDb;
+
 abstract class JobAbstract extends Object implements JobInterface
 {
     public $name;
     public $catalog;
-    public $description;
-    public $execTime;
+
+    public function __construct()
+    {
+        Yii::$app->queue->on(Queue::EVENT_AFTER_PUSH, function ($event) {
+            $id=$event->id;
+            $queue = new QueueDb();
+            $queue->name=$this->getName();
+            $queue->catalog=$this->getCatalog();
+            $queue->description=json_encode($event->job);
+            $queue->exec_time=$event->delay;
+            $queue->queue_id=$event->id;
+            $status=$this->getStatus($id);
+            $queue->status=$status;
+            $queue->save();
+        });
+        Yii::$app->queue->on(Queue::EVENT_AFTER_PUSH, function ($event) {
+           // if ($event->error instanceof TemporaryUnprocessableJobException) {
+                $id=$event->id;
+                $queue = QueueDb::findOne(['queue_id'=>$id]);
+                $queue->status=-1;
+                $queue->save();
+           // }
+        });
+        Yii::$app->queue->on(Queue::EVENT_AFTER_EXEC, function ($event) {
+            $id=$event->id;
+            $queue = QueueDb::findOne(['queue_id'=>$id]);
+            $queue->status=$this->getStatus($id);
+            $queue->save();
+        });
+    }
+
+    public function getStatus($id){
+        if(Yii::$app->queue->isWaiting($id)){
+            $status=1;
+        }elseif(Yii::$app->queue->isReserved($id)){
+            $status=2;
+        }elseif(Yii::$app->queue->isDone($id)){
+            $status=3;
+        }
+        return $status;
+    }
 
     /** name (任务名称)
      * @return mixed
@@ -19,12 +62,4 @@ abstract class JobAbstract extends Object implements JobInterface
      */
     abstract function getCatalog();
 
-    /** description (详情信息)
-     * @return mixed
-     */
-    abstract function getDescription();
-    /**
-     * @return mixed  int  时间戳
-     */
-    abstract function getExecTime();
 }
