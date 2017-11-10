856ef047d0f8a9323a8e49e124990711643500aa
陈家辉 <chenjiahui@haolianluo.com>
2017-11-10 14:54:58 +0800

    ci(redis连接):

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -52,7 +52,7 @@ class TestCase extends \PHPUnit_Framework_TestCase
                 ],
                 'redis' => [
                     'class' => 'yii\redis\Connection',
-                    'hostname' => 'localhost',
+                    'hostname' => '127.0.0.1',
                     'port' => '6379',
                     'database' => 0,
                 ],
